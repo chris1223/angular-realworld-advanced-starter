d4b2c1e14de27ffd9008a2e8aec9210e1e60a772
chris1223 <chris711223@gmail.com>
2020-03-21 13:49:39 +0800

    在程式中取得路由參數

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../article';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  article: Article;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private articleService: PostService) { }
 
   ngOnInit() {
+    this.route.paramMap
+      // .pipe(switchMap(params => params.get('id')), tap(id => (this.id = +id)))
+      .subscribe(params => {
+        this.articleService.getArticle(params.get('id'))
+          .subscribe(article => {
+            this.article = article.article;
+          });
+      });
+
   }
 
 }
