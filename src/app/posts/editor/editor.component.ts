7a977f9dcd7564e1356c089e17ab9010a997ae38
chris1223 <chris711223@gmail.com>
2020-03-21 17:04:18 +0800

    使用 Model-driven Form 繫結表單資料

--- src/app/posts/editor/editor.component.ts
+++ src/app/posts/editor/editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-editor',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
+  form = new FormGroup({
+    title: new FormControl('test'),
+    body: new FormControl(),
+    tags: new FormArray([
+      new FormControl('angular'),
+      new FormControl('HTML')
+    ])
+  });
 
-  constructor() { }
+  form2 = this.fb.group({
+    title: this.fb.control('')
+  });
+
+  get tagsControlls() {
+    return (this.form.get('tags') as FormArray).controls;
+  }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
   }
 
+  addTag(tag: string) {
+    if (!tag) {
+      return;
+    }
+    (this.form.get('tags') as FormArray).push(new FormControl(tag));
+  }
+
+  removeTag(index: number) {
+    (this.form.get('tags') as FormArray).removeAt(index);
+  }
 }
